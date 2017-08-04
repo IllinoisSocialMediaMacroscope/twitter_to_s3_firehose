425e9bd6dcd70190ba59119641e3c7756d155ee9
jmtroy <joseph.troy@gmail.com>
2017-08-04 13:20:49 -0500

    test new repository and update get_node_js_code.sh comments

--- get_node_js_code.sh
+++ get_node_js_code.sh
@@ -1,8 +1,12 @@
 #1/bin/bash/
 
-# get code from below
+# get THE ORIGINAL code from below
 # https://github.com/awslabs/aws-big-data-blog/raw/master/aws-blog-firehose-lambda-elasticsearch-near-real-time-discovery-platform/packages/twitter-streaming-firehose-nodejs.zip
 # and unzip
+
+# get THE social media macroscope code from below
+# https://github.com/IllinoisSocialMediaMacroscope/twitter_to_s3_firehose.git
+
 
 # to install nodejs use the below command (MAC OS/LINUX)
 
