4431c002cb67bb14df466dbd684d43b12ac5433e
jmtroy <joseph.troy@gmail.com>
2017-08-10 13:05:23 -0500

    Send standard out to stdout.txt

--- twitter_stream_producer_app/run_twitter_stream_producer_app_w_nohup.sh
+++ twitter_stream_producer_app/run_twitter_stream_producer_app_w_nohup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Modifications:
+# 08-10-2017 JMT send standard out to stdout.txt
+
 # This simple script will start the twitter stream producer app using nohup
 # run at the command line :$ sh run_twitter_stream_producer_app_w_nohup.sh
 # put this script in the same folder as the "twitter_stream_producer_app"
@@ -13,7 +16,7 @@
 
 # start node twitter_stream_producer_app in the background (nohup) 
 # and send standard out (tweets) to /dev/null (that is, do not store them anywhere on this EC2)
-nohup node twitter_stream_producer_app > /dev/null 2>stderr.txt &
+nohup node twitter_stream_producer_app > stdout.txt 2>stderr.txt &
 # Save the pid, so we can stop it if needed
 echo "nohub pid is: "$! > save_twitter_stream_nohup_pid.txt
 
